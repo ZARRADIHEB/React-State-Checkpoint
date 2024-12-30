9a1ae8125463b861322209edf304214bea5553ad
ZARRADIHEB <126171975+ZARRADIHEB@users.noreply.github.com>
2024-12-30 15:07:00 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import icon from "./icon/avatar.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "John Doe",
+        bio: "Lorem Ipsum",
+        imgSrc: icon,
+        profession: "Software Developer",
+      },
+      shows: false,
+      timeMounted: {
+        seconds: 0,
+        minutes: 0,
+        hours: 0,
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => {
+        const { seconds, minutes, hours } = prevState.timeMounted;
+        let newSeconds = seconds + 1;
+        let newMinutes = minutes;
+        let newHours = hours;
+
+        if (newSeconds === 60) {
+          newSeconds = 0;
+          newMinutes += 1;
+        }
+
+        if (newMinutes === 60) {
+          newMinutes = 0;
+          newHours += 1;
+        }
+
+        return {
+          timeMounted: {
+            seconds: newSeconds,
+            minutes: newMinutes,
+            hours: newHours,
+          },
+        };
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, shows, timeMounted } = this.state;
+    const toggleProfile = () => {
+      this.setState({ shows: !shows });
+    };
+    return (
+      <div className="App">
+        {shows && (
+          <div className="profile">
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt="Avatar" />
+            <h4>{person.profession}</h4>
+          </div>
+        )}
+        <button
+          className={shows ? "true" : "false"}
+          onClick={toggleProfile}
+          type="button"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {shows ? "Hide Profile" : "Show Profile"}
+        </button>
+        <h2>
+          Last Time Component Mounted: {timeMounted.hours}h{" "}
+          {timeMounted.minutes}m {timeMounted.seconds}s
+        </h2>
+      </div>
+    );
+  }
 }
 
 export default App;
